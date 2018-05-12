cc902ead20fe25b711569c10e7476b4727af5a45
Celebro dev <ed.proff@gmail.com>
2018-05-12 16:23:10 +0300

    react 16.3.2

---
+++ index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const https = require('https')
+const fs = require('fs');
+
+const resource = [
+    'https://unpkg.com/react@16.3.2/umd/react.production.min.js',
+    'https://unpkg.com/react@16.3.2/umd/react.development.js',
+    'https://unpkg.com/react-dom@16.3.2/umd/react-dom.development.js',
+    'https://unpkg.com/react@16.3.2/umd/react.production.min.js',
+    'https://unpkg.com/react-dom@16.3.2/umd/react-dom.production.min.js'
+];
+
+const download = function(url) {
+    const file = fs.createWriteStream(path.basename(url));
+
+    https.get(url, (response) => {
+        response.pipe(file);
+
+        file.on('finish', () => {
+            console.log('download complete!');
+        });
+    });
+}
+
+resource.map(url => download(url));
